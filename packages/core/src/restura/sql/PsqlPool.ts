51fed7ab6a54fdfc5481a470b363441e5ebe5ea1
Tanner Burton <tburt713@gmail.com>
2024-10-22 16:14:19 -0600

    feat: support for transactions.

--- packages/core/src/restura/sql/PsqlPool.ts
+++ packages/core/src/restura/sql/PsqlPool.ts
@@ -1,16 +1,14 @@
-import pg from 'pg';
-import format from 'pg-format';
+import pg, { QueryResult, QueryResultRow } from 'pg';
 // See this github issue for why we need to do this https://github.com/brianc/node-postgres/issues/2819
 import type { PoolConfig, Pool as PoolType, QueryConfigValues } from 'pg';
 import { logger } from '../../logger/logger.js';
-import { RsError } from '../errors.js';
-import type { RequesterDetails } from '../types/customExpress.types.js';
-import { questionMarksToOrderedParams } from './PsqlUtils.js';
+import PsqlConnection from './PsqlConnection.js';
 const { Pool } = pg;
 
-export class PsqlPool {
+export class PsqlPool extends PsqlConnection {
 	public pool: PoolType;
 	constructor(public poolConfig: PoolConfig) {
+		super();
 		this.pool = new Pool(poolConfig);
 		// Run a test query to ensure the connection is working
 		this.queryOne('SELECT NOW();', [], { isSystemUser: true, role: '', host: 'localhost', ipAddress: '' })
@@ -23,79 +21,10 @@ export class PsqlPool {
 			});
 	}
 
-	// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-	async queryOne(query: string, options: any[], requesterDetails: RequesterDetails) {
-		const formattedQuery = questionMarksToOrderedParams(query);
-		this.logSqlStatement(formattedQuery, options, requesterDetails);
-
-		try {
-			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-			const response = await this.pool.query(formattedQuery, options as QueryConfigValues<any>);
-			// There should be one and only one row returned
-			if (response.rows.length === 0) throw new RsError('NOT_FOUND', 'No results found');
-			else if (response.rows.length > 1) throw new RsError('DUPLICATE', 'More than one result found');
-
-			return response.rows[0];
-			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-		} catch (error: any) {
-			console.error(error, query, options);
-			if (RsError.isRsError(error)) throw error;
-
-			if (error?.routine === '_bt_check_unique') {
-				throw new RsError('DUPLICATE', error.message);
-			}
-			throw new RsError('DATABASE_ERROR', `${error.message}`);
-		}
-	}
-
-	// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-	async runQuery(query: string, options: any[], requesterDetails: RequesterDetails) {
-		const formattedQuery = questionMarksToOrderedParams(query);
-		this.logSqlStatement(formattedQuery, options, requesterDetails);
-
-		const queryUpdated = query.replace(/[\t\n]/g, ' ');
-		console.log(queryUpdated, options);
-		try {
-			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-			const response = await this.pool.query(formattedQuery, options as QueryConfigValues<any>);
-			return response.rows;
-			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-		} catch (error: any) {
-			console.error(error, query, options);
-			if (error?.routine === '_bt_check_unique') {
-				throw new RsError('DUPLICATE', error.message);
-			}
-			throw new RsError('DATABASE_ERROR', `${error.message}`);
-		}
-	}
-
-	private logSqlStatement(
+	protected async query<R extends QueryResultRow = any>(
 		query: string,
-		// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-		options: any[],
-		requesterDetails: RequesterDetails,
-		prefix: string = ''
-	) {
-		if (logger.level !== 'silly') return;
-
-		let sqlStatement = '';
-		if (options.length === 0) {
-			sqlStatement = query;
-		} else {
-			// The string will contain $1, $2, etc for each parameter, so we need to replace them with the actual values
-			let stringIndex = 0;
-			sqlStatement = query.replace(/\$\d+/g, () => {
-				const value = options[stringIndex++];
-				if (typeof value === 'number') return value.toString();
-				return format.literal(value);
-			});
-		}
-
-		let initiator = 'Anonymous';
-		if ('userId' in requesterDetails && requesterDetails.userId)
-			initiator = `User Id (${requesterDetails.userId.toString()})`;
-		if ('isSystemUser' in requesterDetails && requesterDetails.isSystemUser) initiator = 'SYSTEM';
-
-		logger.silly(`${prefix}query by ${initiator}, Query ->\n ${sqlStatement}`);
+		values?: QueryConfigValues<any>
+	): Promise<QueryResult<R>> {
+		return this.pool.query(query, values) as Promise<QueryResult<R>>;
 	}
 }
