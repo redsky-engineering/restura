d9a655e8f12ee21f502c8fd9d030bdd66b1ec724
Tanner Burton <tburt713@gmail.com>
2024-10-29 11:22:31 -0600

    fix: typo

--- packages/core/src/config.schema.ts
+++ packages/core/src/config.schema.ts
@@ -9,7 +9,7 @@ export const resturaConfigSchema = z.object({
 	authToken: z.string().min(1, 'Missing Restura Auth Token'),
 	sendErrorStackTrace: z.boolean().default(false),
 	schemaFilePath: z.string().default(process.cwd() + '/restura.schema.json'),
-	customApiFolderPath: z.string().default(process.cwd() + '/src/api'), // changed to src directory since dist is compiled to a few files without the api direcotry
+	customApiFolderPath: z.string().default(process.cwd() + '/src/api'), // changed to src directory since dist is compiled to a few files without the api directory
 	generatedTypesPath: z.string().default(process.cwd() + '/src/@types')
 });
 export type ResturaConfigSchema = z.infer<typeof resturaConfigSchema>;
