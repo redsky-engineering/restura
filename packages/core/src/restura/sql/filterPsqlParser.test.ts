13889b794300cd900e95e8c9bb14616493554b54
burtontanner <tburt713@gmail.com>
2024-10-30 16:40:45 -0600

    feat: support for values to have whitespace (#28)
    * feat: support for values to have whitespace
    
    * feat: support for spaces throughout the query.

--- packages/core/src/restura/sql/filterPsqlParser.test.ts
+++ packages/core/src/restura/sql/filterPsqlParser.test.ts
@@ -1,18 +1,20 @@
 import { expect, assert } from 'chai';
 import { Done } from 'mocha';
 import filterPsqlParser from './filterPsqlParser.js';
-const trimRedundantWhitespace = (str: string) => str.replace(/\s+/g, ' ').trim();
-function test(inputString: string, expectedString: string) {
+function test(inputString: string, expectedString: string, testName?: string) {
 	const result: string = filterPsqlParser.parse(inputString);
-	expect(trimRedundantWhitespace(result))
-		.to.exist.with.length.greaterThan(0)
-		.and.is.equal(trimRedundantWhitespace(expectedString));
+	try {
+		expect(result).to.exist.with.length.greaterThan(0).and.is.equal(expectedString);
+	} catch (e) {
+		if (testName) console.error(testName, 'failed');
+		throw e;
+	}
 }
 
-function testBadInput(inputString: string) {
+function testBadInput(inputString: string, testName?: string) {
 	try {
 		filterPsqlParser.parse(inputString);
-		assert.fail(`The parsing should have failed on ${inputString}`);
+		assert.fail(`The parsing should have failed on ${inputString} ` + testName);
 		// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 	} catch (e: any) {
 		if (e.name !== 'SyntaxError') {
@@ -25,10 +27,42 @@ describe('Filter Psql Parsing test', function () {
 	it('Should parse valid strings', function (done: Done) {
 		test('!(column:id,value:4504055,type:contains)', ` NOT ("id" ILIKE '%4504055%')`);
 		test('!(column:id,value:4504055,type:startsWith)', ` NOT ("id" ILIKE '4504055%')`);
+		test('(column:id,value:Tanner B,type:startsWith)', `("id" ILIKE 'Tanner B%')`, 'allow space');
+		test('(column: id ,value: Tanner B ,type:startsWith)', `(" id " ILIKE ' Tanner B %')`, 'allow space');
+		test('(column:id,value:Tanner  B,type:startsWith)', `("id" ILIKE 'Tanner  B%')`, 'allow spaces');
+		test(`(column:id,value:Tanner	B,type:startsWith)`, `("id" ILIKE 'Tanner	B%')`, 'allow tab');
+		test(
+			`(column:id,value:Tanner
+B,type:startsWith)`,
+			`("id" ILIKE 'Tanner
+B%')`,
+			'allow newline'
+		);
 		test(
 			'!(column:id,value:4504055,type:contains)and!(column:name,value:jim,type:endsWith)',
-			` NOT ("id" ILIKE '%4504055%') and NOT ("name" ILIKE '%jim')`
+			` NOT ("id" ILIKE '%4504055%') and  NOT ("name" ILIKE '%jim')`
 		);
+		test(
+			'!(column:id,value:4504055,type:contains)   and!(column:name,value:jim,type:endsWith)',
+			` NOT ("id" ILIKE '%4504055%') and  NOT ("name" ILIKE '%jim')`,
+			'allow but trim whitespace'
+		);
+		test(
+			'! ( column :id, value :4504055,  type: contains )   and ! ( column :name, value :jim, type : endsWith ) ',
+			` NOT ("id" ILIKE '%4504055%') and  NOT ("name" ILIKE '%jim')`,
+			'allow but trim whitespace'
+		);
+		test(
+			'!(column:id,value:4504055,type :contains)   and!(column:name,value:jim,type:endsWith)',
+			` NOT ("id" ILIKE '%4504055%') and  NOT ("name" ILIKE '%jim')`,
+			'allow but trim whitespace'
+		);
+		test(
+			'!(column:id,value:4504055 ,  type:contains)   and!(column:name,value:jim,type:endsWith)',
+			` NOT ("id" ILIKE '%4504055 %') and  NOT ("name" ILIKE '%jim')`,
+			'allow but trim whitespace'
+		);
+
 		test('(((column:id,value:4504055,type:contains)))', `((("id" ILIKE '%4504055%')))`);
 
 		test('(column:id,value:15234,type:exact)', `("id" = '15234')`);
@@ -36,7 +70,7 @@ describe('Filter Psql Parsing test', function () {
 		test('!(column:userId,value:15234,type:exact)', ` NOT ("userId" = '15234')`);
 		test(
 			'!(!(column:userId,value:15234,type:exact)and!(column:name,value:jim,type:startsWith))or(column:name,value:bob)',
-			` NOT ( NOT ("userId" = '15234') and NOT ("name" ILIKE 'jim%')) or ("name" = 'bob')`
+			` NOT ( NOT ("userId" = '15234') and  NOT ("name" ILIKE 'jim%')) or ("name" = 'bob')`
 		);
 
 		test('(column:id,value:251,type:contains)', `("id" ILIKE '%251%')`);
@@ -63,20 +97,20 @@ describe('Filter Psql Parsing test', function () {
 		);
 		test(
 			'(column:id,value:215)AND!(column:totalPriceCents,value:3070,type:greaterThan)',
-			`("id" = '215') AND NOT ("totalPriceCents" > '3070')`
+			`("id" = '215') AND  NOT ("totalPriceCents" > '3070')`
 		);
 		test(
 			'(column:id,value:215)AND!(column:totalPriceCents,value:3071,type:lessThan)',
-			`("id" = '215') AND NOT ("totalPriceCents" < '3071')`
+			`("id" = '215') AND  NOT ("totalPriceCents" < '3071')`
 		);
 		test(
 			'(column:id,value:215)AND!(column:totalPriceCents,value:3071,type:lessThanEqual)',
-			`("id" = '215') AND NOT ("totalPriceCents" <= '3071')`
+			`("id" = '215') AND  NOT ("totalPriceCents" <= '3071')`
 		);
 
 		test(
 			'(column:id,value:215)AND!(column:totalPriceCents,value:3071,type:lessThan)',
-			`("id" = '215') AND NOT ("totalPriceCents" < '3071')`
+			`("id" = '215') AND  NOT ("totalPriceCents" < '3071')`
 		);
 		test('(column:orderV2.id,value:215)', `("orderV2"."id" = '215')`);
 		test(
@@ -86,12 +120,12 @@ describe('Filter Psql Parsing test', function () {
 
 		test(
 			'(column:id)AND!(column:totalPriceCents,value:3071,type:lessThan)',
-			`("id" = NULL) AND NOT ("totalPriceCents" < '3071')`
+			`("id" = NULL) AND  NOT ("totalPriceCents" < '3071')`
 		);
 
 		test(
 			'(column:id,type:isNull)AND!(column:totalPriceCents,value:3071,type:lessThan)',
-			`(isNull("id")) AND NOT ("totalPriceCents" < '3071')`
+			`(isNull("id")) AND  NOT ("totalPriceCents" < '3071')`
 		);
 
 		done();
