e99770bb0a0d39e1c396f86d1e42a994cc055078
Atraue Wallis <atraue@redskyeng.com>
2024-11-25 14:14:44 -0700

    refactor: use GENERATED ALWAYS

--- apps/restura-express/migrations/data.sql
+++ apps/restura-express/migrations/data.sql
@@ -1,26 +1,29 @@
--- Avoid notice from truncating tables with foreign keys
+-- Avoid notice from truncating tables with foreign keys SET client_min_messages = WARNING;
 SET client_min_messages = WARNING;
 
 -- Remove all old data
 DO $$
 DECLARE
-    table_record RECORD;
+	table_record RECORD;
 BEGIN
-    FOR table_record IN 
-        SELECT tablename 
-        FROM pg_tables 
-        WHERE schemaname = 'public'
-    LOOP
-        EXECUTE 'TRUNCATE TABLE ' || quote_ident(table_record.tablename) || ' CASCADE;';
-    END LOOP;
-END $$;
+	FOR table_record IN
+	SELECT
+		tablename
+	FROM
+		pg_tables
+	WHERE
+		schemaname = 'public' LOOP
+			EXECUTE 'TRUNCATE TABLE ' || quote_ident(table_record.tablename) || ' CASCADE;';
+		END LOOP;
+END
+$$;
 
 -- Add new data
-INSERT INTO public.company (id, "createdOn", "modifiedOn", name)
-		VALUES(1, '2024-10-13 17:35:45.989731 +00:00', '2024-10-13 17:35:45.989731 +00:00', 'redsky');
+INSERT INTO public.company ("createdOn", "modifiedOn", name)
+		VALUES('2024-10-13 17:35:45.989731 +00:00', '2024-10-13 17:35:45.989731 +00:00', 'redsky');
 
-INSERT INTO public. "user" (id, "createdOn", "modifiedOn", "firstName", "lastName", "companyId", PASSWORD, email, ROLE, "permissionLogin", "lastLoginOn", phone, "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", metadata)
-		VALUES(1, '2024-10-08 20:53:30.663673 +00:00', '2024-10-29 22:38:16.029000 +00:00', 'Tanner', 'Burton', 1, 'asdfa', 'tanner@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}');
+INSERT INTO public. "user" ("createdOn", "modifiedOn", "firstName", "lastName", "companyId", PASSWORD, email, ROLE, "permissionLogin", "lastLoginOn", phone, "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", metadata)
+		VALUES('2024-10-08 20:53:30.663673 +00:00', '2024-10-29 22:38:16.029000 +00:00', 'Tanner', 'Burton', 1, 'asdfa', 'tanner@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}');
 
-INSERT INTO public. "user" (id, "createdOn", "modifiedOn", "firstName", "lastName", "companyId", PASSWORD, email, ROLE, "permissionLogin", "lastLoginOn", phone, "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", metadata)
-		VALUES(12, '2024-10-08 20:55:45.580415 +00:00', '2024-10-08 20:55:45.580415 +00:00', 'Billy', 'Bob', 1, 'asdfa', 'billy1@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}');
+INSERT INTO public. "user" ("createdOn", "modifiedOn", "firstName", "lastName", "companyId", PASSWORD, email, ROLE, "permissionLogin", "lastLoginOn", phone, "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", metadata)
+		VALUES('2024-10-08 20:55:45.580415 +00:00', '2024-10-08 20:55:45.580415 +00:00', 'Billy', 'Bob', 1, 'asdfa', 'billy1@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}');
