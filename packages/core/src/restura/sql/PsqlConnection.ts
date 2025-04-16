ad393d2f0c90c7d13250c088105aa863349e6420
Joshua Hintze <joshua.hintze@gmail.com>
2025-04-16 12:19:09 -0600

    feat: make logging statements consistent using a formatting library. Also perform timing

--- packages/core/src/restura/sql/PsqlConnection.ts
+++ packages/core/src/restura/sql/PsqlConnection.ts
@@ -1,6 +1,7 @@
 import crypto, { UUID } from 'crypto';
 import { QueryConfigValues, QueryResult, QueryResultRow } from 'pg';
 import format from 'pg-format';
+import { format as sqlFormat } from 'sql-formatter';
 import { logger } from '../../logger/logger.js';
 import { RsError } from '../RsError.js';
 import { QueryMetadata } from '../eventManager.js';
@@ -25,9 +26,17 @@ export abstract class PsqlConnection {
 		this.logSqlStatement(formattedQuery, options, meta);
 		const queryMetadata = `--QUERY_METADATA(${JSON.stringify(meta)})\n`;
 
+		const startTime = process.hrtime();
 		try {
 			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 			const response = await this.query(queryMetadata + formattedQuery, options as QueryConfigValues<any>);
+
+			if (logger.level === 'silly') {
+				const [seconds, nanoseconds] = process.hrtime(startTime);
+				const duration = seconds * 1000 + nanoseconds / 1000000;
+				logger.silly(`Query duration: ${duration.toFixed(2)}ms`);
+			}
+
 			// There should be one and only one row returned
 			if (response.rows.length === 0) throw new RsError('NOT_FOUND', 'No results found');
 			else if (response.rows.length > 1) throw new RsError('DUPLICATE', 'More than one result found');
@@ -50,9 +59,17 @@ export abstract class PsqlConnection {
 		const meta: QueryMetadata = { connectionInstanceId: this.instanceId, ...requesterDetails };
 		this.logSqlStatement(formattedQuery, options, meta);
 		const queryMetadata = `--QUERY_METADATA(${JSON.stringify(meta)})\n`;
+		const startTime = process.hrtime();
 		try {
 			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 			const response = await this.query(queryMetadata + formattedQuery, options as QueryConfigValues<any>);
+
+			if (logger.level === 'silly') {
+				const [seconds, nanoseconds] = process.hrtime(startTime);
+				const duration = seconds * 1000 + nanoseconds / 1000000;
+				logger.silly(`Query duration: ${duration.toFixed(2)}ms`);
+			}
+
 			return response.rows as T[];
 			// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 		} catch (error: any) {
@@ -76,7 +93,6 @@ export abstract class PsqlConnection {
 		if (options.length === 0) {
 			sqlStatement = query;
 		} else {
-			// The string will contain $1, $2, etc for each parameter, so we need to replace them with the actual values
 			let stringIndex = 0;
 			sqlStatement = query.replace(/\$\d+/g, () => {
 				const value = options[stringIndex++];
@@ -85,11 +101,20 @@ export abstract class PsqlConnection {
 			});
 		}
 
+		const formattedSql = sqlFormat(sqlStatement, {
+			language: 'postgresql',
+			linesBetweenQueries: 2,
+			indentStyle: 'standard',
+			keywordCase: 'upper',
+			useTabs: true,
+			tabWidth: 4
+		});
+
 		let initiator = 'Anonymous';
 		if ('userId' in queryMetadata && queryMetadata.userId)
 			initiator = `User Id (${queryMetadata.userId.toString()})`;
 		if ('isSystemUser' in queryMetadata && queryMetadata.isSystemUser) initiator = 'SYSTEM';
 
-		logger.silly(`${prefix}query by ${initiator}, Query ->\n ${sqlStatement}`);
+		logger.silly(`${prefix}query by ${initiator}, Query ->\n${formattedSql}`);
 	}
 }
