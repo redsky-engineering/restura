5633d8fc4e9c9978d73c392a91e3178180b00bc4
Suess <alex@redskytech.io>
2025-11-17 13:38:40 -0700

    fix: enhance error handling in errorHandler to support multiple error types
    Updated the errorHandler function to accept both RsErrorInternalData and generic Error types. Improved error response logic to differentiate between RsError and generic Error instances, ensuring appropriate error messages and statuses are sent in the response.

--- apps/restura-express/src/errorHandler.ts
+++ apps/restura-express/src/errorHandler.ts
@@ -9,7 +9,7 @@ import {
 import type { NextFunction } from 'express';
 
 export default function errorHandler(
-	err: RsErrorInternalData,
+	err: RsErrorInternalData | Error,
 	_req: RsRequest<unknown>,
 	res: RsResponse<unknown>,
 	next: NextFunction
@@ -21,15 +21,25 @@ export default function errorHandler(
 
 	logger.error(JSON.stringify(err));
 
-	if (err.err === 'CONNECTION_ERROR') {
-		// Let the server die, so it will try to reconnect to database
-		// THIS SHOULD NOT BE AN RsError.
-		throw new RsError('CONNECTION_ERROR');
+	if (err instanceof RsError) {
+		// Handle err's thar are of type RsError.
+
+		if (err.err === 'CONNECTION_ERROR') {
+			// Let the server die, so it will try to reconnect to database
+			// THIS SHOULD NOT BE AN RsError.
+			throw new RsError('CONNECTION_ERROR');
+		}
 	}
 
 	// If an API request has invalid body we could potentially fail before we have even setup the sendError function
 	// thus lets check if its available first.
 	if (res.sendError) {
-		res.sendError(err.err, err.msg || err.message || 'error', err.status, err.stack);
+		if (err instanceof RsError) {
+			res.sendError(err.err, err.msg || 'error', err.status, err.stack);
+		} else if (err instanceof Error) {
+			res.sendError('UNKNOWN_ERROR', err.message || 'A server error occurred', 500, err.stack);
+		} else {
+			res.sendError('SERVER_ERROR', 'A server error occurred', 500, err.stack);
+		}
 	} else res.status(HtmlStatusCodes.SERVER_ERROR).send({ err: 'SERVER_ERROR', msg: 'A server error occurred' });
 }
