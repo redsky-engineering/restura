d214005459a22840d8bbffa788c09ea5c3d01bf7
Atraue Wallis <atraue@redskyeng.com>
2025-12-26 16:15:26 -0800

    fix: log correct array value types in logSqlStatement

--- packages/core/src/restura/sql/PsqlConnection.ts
+++ packages/core/src/restura/sql/PsqlConnection.ts
@@ -132,6 +132,9 @@ export abstract class PsqlConnection {
 				const value = options[paramIndex];
 				if (typeof value === 'number') return value.toString();
 				if (typeof value === 'boolean') return value.toString();
+				if (Array.isArray(value)) {
+					return `ARRAY[${value.join(', ')}]`;
+				}
 				return format.literal(value as string | object | Date | null | undefined);
 			});
 		}
