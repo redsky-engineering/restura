8413e367874c21339cb4f8059c19b80229f4f354
Atraue Wallis <atraue@redskyeng.com>
2025-12-31 10:35:31 -0800

    chore: remove commented out code

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -95,47 +95,7 @@ const config: Config = {
 		},
 		footer: {
 			style: 'dark',
-			links: [
-				// {
-				// 	title: 'Docs',
-				// 	items: [
-				// 		{
-				// 			label: 'Tutorial',
-				// 			to: '/docs/intro'
-				// 		}
-				// 	]
-				// },
-				// {
-				// 	title: 'Community',
-				// 	items: [
-				// 		{
-				// 			label: 'Stack Overflow',
-				// 			href: 'https://stackoverflow.com/questions/tagged/restura'
-				// 		},
-				// 		{
-				// 			label: 'Discord',
-				// 			href: 'https://discordapp.com/invite/docusaurus'
-				// 		},
-				// 		{
-				// 			label: 'X',
-				// 			href: 'https://x.com/docusaurus'
-				// 		}
-				// 	]
-				// },
-				// {
-				// 	title: 'More',
-				// 	items: [
-				// 		{
-				// 			label: 'Blog',
-				// 			to: '/blog'
-				// 		},
-				// 		{
-				// 			label: 'GitHub',
-				// 			href: 'https://github.com/facebook/docusaurus'
-				// 		}
-				// 	]
-				// }
-			],
+			links: [],
 			copyright: `Copyright © ${new Date().getFullYear()} RedSky Engineering.<br>Built with Docusaurus.`
 		},
 		prism: {
