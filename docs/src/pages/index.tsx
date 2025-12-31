ef0650197ce449b0850b3f7d2e2b41f48fb9c695
Atraue Wallis <atraue@redskyeng.com>
2025-12-31 10:35:02 -0800

    chore: update title to restura

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import type { ReactNode } from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import type { ReactNode } from 'react';
 
 import styles from './index.module.css';
 
@@ -19,7 +19,7 @@ function HomepageHeader() {
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
 					<Link className="button button--secondary button--lg" to="/docs/intro">
-						Docusaurus Tutorial - 5min ⏱️
+						Restura Tutorial - 5min ⏱️
 					</Link>
 				</div>
 			</div>
