125696a6928ab95f43aa3771b164fc5c2e640564
Joshua Hintze <joshua.hintze@gmail.com>
2026-02-01 16:15:38 -0700

    fix: date column type conversion to simple string instead of Date type

--- apps/restura-express/migrations/data.sql
+++ apps/restura-express/migrations/data.sql
@@ -22,11 +22,11 @@ $$;
 INSERT INTO public.company ("createdOn", "modifiedOn", name)
 VALUES('2024-10-13 17:35:45.989731 +00:00', '2024-10-13 17:35:45.989731 +00:00', 'redsky');
 
-INSERT INTO public. "user" ("createdOn", "modifiedOn", "syncVersion", "firstName", "lastName", "companyId", "password", "email", "role", "permissionLogin", "lastLoginOn", "phone", "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", "metadata")
-VALUES('2024-10-08 20:53:30.663673 +00:00', '2024-10-29 22:38:16.029000 +00:00', 1, 'Tanner', 'Burton', 1, 'asdfa', 'tanner@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}');
+INSERT INTO public. "user" ("createdOn", "modifiedOn", "syncVersion", "firstName", "lastName", "companyId", "password", "email", "role", "permissionLogin", "lastLoginOn", "phone", "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", "metadata", "birthDate")
+VALUES('2024-10-08 20:53:30.663673 +00:00', '2024-10-29 22:38:16.029000 +00:00', 1, 'Tanner', 'Burton', 1, 'asdfa', 'tanner@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}', '1990-05-15');
 
-INSERT INTO public. "user" ("createdOn", "modifiedOn", "syncVersion", "firstName", "lastName", "companyId", "password", "email", "role", "permissionLogin", "lastLoginOn", "phone", "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", "metadata")
-VALUES('2024-10-08 20:55:45.580415 +00:00', '2024-10-08 20:55:45.580415 +00:00', 1, 'Billy', 'Bob', 1, 'asdfa', 'billy1@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}');
+INSERT INTO public. "user" ("createdOn", "modifiedOn", "syncVersion", "firstName", "lastName", "companyId", "password", "email", "role", "permissionLogin", "lastLoginOn", "phone", "loginDisabledOn", "passwordResetGuid", "verifyEmailPin", "verifyEmailPinExpiresOn", "accountStatus", "passwordResetExpiresOn", "onboardingStatus", "pendingEmail", "testAge", "metadata", "birthDate")
+VALUES('2024-10-08 20:55:45.580415 +00:00', '2024-10-08 20:55:45.580415 +00:00', 1, 'Billy', 'Bob', 1, 'asdfa', 'billy1@plvr.com', 'user', TRUE, NULL, NULL, NULL, NULL, NULL, NULL, 'view_only', NULL, 'verify_email', NULL, 0, '{}', '1985-12-25');
 
 INSERT INTO public."order" (id, "createdOn", "modifiedOn", "userId", "amountCents") VALUES (1, '2025-12-15 22:39:06.171363 +00:00', '2025-12-15 22:39:06.171363 +00:00', 1, 50);
 
